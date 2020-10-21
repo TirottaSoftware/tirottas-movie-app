5a65d94ce1b7df0e7d445f56fce138a0e85519f4
I449915 <c.cristiantirotta@student.fontys.nl>
2020-10-21 13:24:17 +0200

    Updates

---
+++ precache-manifest.eefa3bd8743c0f56c1a3e99c37e2d0ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e07bf1a6668aa665143d320fe9c4fd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "862ddfd64496d0008133",
+    "url": "/static/css/main.81aadf6b.chunk.css"
+  },
+  {
+    "revision": "979dd641d3f0be1a8706",
+    "url": "/static/js/2.e3ff9f7b.chunk.js"
+  },
+  {
+    "revision": "b948d2a7727b83687a00ec53ef5267c0",
+    "url": "/static/js/2.e3ff9f7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "862ddfd64496d0008133",
+    "url": "/static/js/main.e7e132e0.chunk.js"
+  },
+  {
+    "revision": "c0df796a591b1bb88eba",
+    "url": "/static/js/runtime-main.cc15f62b.js"
+  }
+]);
